6500956d1a749719f15e5c0a6e5884d4335e3b5c
vcho1958 <gusckd57@gmail.com>
2022-04-26 18:08:14 +0900

    영속성 컨텍스트

--- src/main/java/hello/JpaMain.java
+++ src/main/java/hello/JpaMain.java
@@ -21,18 +21,25 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-
+// 모든 DB 작업은 쓰기 지연 SQL 저장소에 쌓인 후 tx.commit()과정에서 시작함
+//1차 캐시에 존재할 경우 캐시에서 조회
+            //해당 시점에서 1차캐시의 스냅샷을 비교하여 다르다면 적절하게 update쿼리 날림
 //            Member member = em.find(Member.class, 1L); //find 메소드
 //            System.out.println("id =" + member.getId());
 //            System.out.println("name = "+ member.getName());
 //            member.setName("updatedName");//이름 변경
-//            em.persist(member); //해당 인스턴스 DB추적 시작
+//            em.persist(member); //해당 인스턴스 DB추적 시작 em의 1차 캐시에 저장
 //             em.remove(member); // 해당 인스턴스 삭제
+            Member member = em.find(Member.class, "member1");
+            //1차 캐시에서 조회 후 없으면 DB를 조회하여 있다면 1차캐시에 저장 후 반환
+            //따라서 같은 객체를 불러오는 쿼리가 요구될 경우 캐시에서 조회하여 DB쿼리 절감
+            //call by reference
             List<Member> result = em.createQuery("select m from Member as m", Member.class)
                     .setFirstResult(5)//skip
                     .setMaxResults(8)//limit
                     .getResultList();
-            for(Member member: result)System.out.println("member.name = " + member.getName());
+//            em.detach(member);//DB추적 분리
+//            em.remove(member);//제거
             tx.commit();
         }catch (Exception e){
             tx.rollback();//롤백
