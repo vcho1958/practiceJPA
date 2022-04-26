3919c069537723b77b07bba615af1230f0029f79
vcho1958 <gusckd57@gmail.com>
2022-04-26 18:28:59 +0900

    객체와 테이블 매핍

--- src/main/java/hello/Member.java
+++ src/main/java/hello/Member.java
@@ -2,9 +2,15 @@ package hello;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+//@Entity(name = "JPA에서 인식할 이름(쿼리 등에서)")
+//기본 생성자 필수
+//final 클래스, enum, interface, inner클래스 사용 x
+// 저장할 필드에 final 사용 x
 //@Table(name = "USER") 테이블 명 별도 지정
+//@Table(catalog ,schema = ,uniqueConstraints = )
 public class Member {
     public Long getId() {
         return id;
